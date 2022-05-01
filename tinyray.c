256249d23257fbb14fee132235e3b1bca44ea387
Lesslyrics <alina_boshenko@mail.ru>
2022-05-01 21:49:54 +0200

    added algo optimization

--- tinyray.c
+++ tinyray.c
@@ -10,9 +10,11 @@
 //  - https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "stb_image_write.h"
 #include <math.h>
 
+#define SAMPLES 4
 // A single byte-type representing one channel of a pixel
 typedef unsigned char byte;
 
@@ -44,23 +46,23 @@ Vec3 vec3_subtract_scalar(Vec3 a, float b) {
 }
 
 Vec3 vec3_divide_scalar(Vec3 a, float b) {
-    return vec3_new(a.x/b, a.y/b, a.z/b);
+    return vec3_new(a.x / b, a.y / b, a.z / b);
 }
 
 Vec3 vec3_multiply(Vec3 a, Vec3 b) {
-    return vec3_new(a.x*b.x, a.y*b.y, a.z*b.z);
+    return vec3_new(a.x * b.x, a.y * b.y, a.z * b.z);
 }
 
 Vec3 vec3_multiply_scalar(Vec3 a, float b) {
-    return vec3_new(a.x*b, a.y*b, a.z*b);
+    return vec3_new(a.x * b, a.y * b, a.z * b);
 }
 
 float vec3_dot(Vec3 a, Vec3 b) {
-    return a.x*b.x + a.y*b.y + a.z*b.z;
+    return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
 float vec3_len(Vec3 v) {
-    return (float)sqrtf((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+    return (float) sqrtf((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
 }
 
 Vec3 vec3_normalise(Vec3 v) {
@@ -69,9 +71,9 @@ Vec3 vec3_normalise(Vec3 v) {
 
 Vec3 vec3_clamp(Vec3 v, float min_value, float max_value) {
     return vec3_new(
-        fmin(fmax(v.x, min_value), max_value),
-        fmin(fmax(v.y, min_value), max_value),
-        fmin(fmax(v.z, min_value), max_value)
+            fmin(fmax(v.x, min_value), max_value),
+            fmin(fmax(v.y, min_value), max_value),
+            fmin(fmax(v.z, min_value), max_value)
     );
 }
 // END - vectors
@@ -173,41 +175,35 @@ Ray ray_new(Vec3 origin, Vec3 direction) {
 float lighting_compute(Vec3 point, Vec3 normal,
                        Vec3 view, float specular,
                        Light *lights, int num_lights) {
-    
+
     // Intensity of light for the given pixel
     float intensity = 0.0f;
 
     // Iterate over lights
-    for (int i = 0; i < num_lights; i++)
-    {
+    for (int i = 0; i < num_lights; i++) {
         Light *light = &lights[i];
-        if (light->type == AmbientLight)
-        {
+        if (light->type == AmbientLight) {
             // Simply add ambient light to total
             intensity += light->intensity;
-        }
-        else
-        {
+        } else {
             Vec3 light_ray;
             if (light->type == PointLight)
                 // Point Light: Direction of ray from light to point
                 light_ray = vec3_subtract(light->position, point);
             else
                 // Directional Light: Direction
-                light_ray = light->direction;		
+                light_ray = light->direction;
 
             // Diffuse
             float reflect = vec3_dot(normal, light_ray);
-            intensity += (light->intensity * reflect)/(vec3_len(normal) * vec3_len(light_ray));
+            intensity += (light->intensity * reflect) / (vec3_len(normal) * vec3_len(light_ray));
 
             // Specular
-            if (specular != -1)
-            {
+            if (specular != -1) {
                 Vec3 r = vec3_subtract(vec3_multiply_scalar(normal, 2 * vec3_dot(normal, light_ray)), light_ray);
                 float reflect_view_proj = vec3_dot(r, view);
-                if (reflect_view_proj > 0)
-                {
-                    float cosine = reflect_view_proj/(vec3_len(r) * vec3_len(view));
+                if (reflect_view_proj > 0) {
+                    float cosine = reflect_view_proj / (vec3_len(r) * vec3_len(view));
                     intensity += light->intensity * powf(cosine, specular);
                 }
             }
@@ -227,7 +223,7 @@ float lighting_compute(Vec3 point, Vec3 normal,
 //  - boolean of whether ray intersected a sphere
 //  - [out] dist0, dist 1 = perpendicular distances to points of intersection
 int do_sphere_raycast(Sphere sphere, Ray ray, float *dist0, float *dist1) {
-    
+
     // Please see sphere_ray_intersection.bmp
     *dist0 = 0;
     *dist1 = 0;
@@ -259,7 +255,7 @@ int do_sphere_raycast(Sphere sphere, Ray ray, float *dist0, float *dist1) {
 
     *dist0 = t0;
     *dist1 = t1;
-    
+
     return 1; // Intersection found
 }
 
@@ -279,34 +275,30 @@ RgbColour raytrace(Vec3 origin, Vec3 dir, float min_t, float max_t,
 
     // Closest sphere to screen (for depth-testing)
     Sphere *closest = 0;
-    
+
     // We use t_comp to store the t-depth of the closest
     // sphere and compare it with other spheres to perform
     // primitive depth testing (where 't' is perpendicular
     // distance to the point of intersection)
-    float t_comp = (float)MAX_DIST;
+    float t_comp = (float) MAX_DIST;
 
     // Ray to test
     Ray ray = ray_new(origin, dir);
 
     // Cycle through all spheres and depth-test
-    for (int i = 0; i < num_spheres; i++)
-    {
+    for (int i = 0; i < num_spheres; i++) {
         float dist0, dist1;
-        if (do_sphere_raycast(spheres[i], ray, &dist0, &dist1))
-        {
+        if (do_sphere_raycast(spheres[i], ray, &dist0, &dist1)) {
             // Check dist0
             if ((min_t < dist0 && dist0 < max_t) &&
-                dist0 < t_comp)
-            {
+                dist0 < t_comp) {
                 t_comp = dist0;
                 closest = &spheres[i];
             }
 
             // Now check dist1
             if ((min_t < dist1 && dist1 < max_t) &&
-                dist1 < t_comp)
-            {
+                dist1 < t_comp) {
                 t_comp = dist1;
                 closest = &spheres[i];
             }
@@ -321,16 +313,16 @@ RgbColour raytrace(Vec3 origin, Vec3 dir, float min_t, float max_t,
     Material material = closest->material;
 
     return vec3_clamp(
-        vec3_multiply_scalar(
-            material.diffuse,
-            lighting_compute(
-                point, normal,
-                vec3_multiply_scalar(dir, -1),
-                material.specular,
-                lights, num_lights
-            )
-        ),
-        0.0f, 255.0f // Clamp between 0 and 255
+            vec3_multiply_scalar(
+                    material.diffuse,
+                    lighting_compute(
+                            point, normal,
+                            vec3_multiply_scalar(dir, -1),
+                            material.specular,
+                            lights, num_lights
+                    )
+            ),
+            0.0f, 255.0f // Clamp between 0 and 255
     );
 }
 
@@ -338,13 +330,13 @@ RgbColour raytrace(Vec3 origin, Vec3 dir, float min_t, float max_t,
 #define MARK_COLS 33
 #define MARK_ROWS 7
 int mark[MARK_ROWS][MARK_COLS] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0},
-    {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 };
 
 // Draws a watermark on the screen using the above array
@@ -357,21 +349,20 @@ void DrawWatermark(byte *data) {
 
     for (int i = 0; i < MARK_COLS; i++) {
         for (int j = 0; j < MARK_ROWS; j++) {
-            int y_corner = start_y + j*(size*2 + stride);
-            int x_corner = start_x + i*(size*2 + stride);
+            int y_corner = start_y + j * (size * 2 + stride);
+            int x_corner = start_x + i * (size * 2 + stride);
 
             // Draw Square
             for (int x = x_corner; x < (x_corner + size); x++) {
                 for (int y = y_corner; y < (y_corner + size); y++) {
                     if (mark[j][i] == 0) {
-                        data[(y*WIDTH + x) * 3 + 0] = (byte)(i/(float)MARK_COLS * 255) % 180;
-                        data[(y*WIDTH + x) * 3 + 1] = (byte)(j/(float)MARK_ROWS * 255) % 180;
-                        data[(y*WIDTH + x) * 3 + 2] = (byte)240;
-                    }
-                    else {
-                        data[(y*WIDTH + x) * 3 + 0] = (byte)255;
-                        data[(y*WIDTH + x) * 3 + 1] = (byte)255;
-                        data[(y*WIDTH + x) * 3 + 2] = (byte)255;
+                        data[(y * WIDTH + x) * 3 + 0] = (byte) (i / (float) MARK_COLS * 255) % 180;
+                        data[(y * WIDTH + x) * 3 + 1] = (byte) (j / (float) MARK_ROWS * 255) % 180;
+                        data[(y * WIDTH + x) * 3 + 2] = (byte) 240;
+                    } else {
+                        data[(y * WIDTH + x) * 3 + 0] = (byte) 255;
+                        data[(y * WIDTH + x) * 3 + 1] = (byte) 255;
+                        data[(y * WIDTH + x) * 3 + 2] = (byte) 255;
                     }
                 }
             }
@@ -379,8 +370,7 @@ void DrawWatermark(byte *data) {
     }
 }
 
-int main(void)
-{
+int main(void) {
     byte *data = malloc(sizeof(byte) * 3 * WIDTH * HEIGHT);
 
     // Materials
@@ -390,7 +380,7 @@ int main(void)
     Material ground = material_new(vec3_new(0, 57, 89), 1000);
 
     // Scene
-    #define NUM_SPHERES 4
+#define NUM_SPHERES 4
     Sphere spheres[NUM_SPHERES];
     spheres[0] = sphere_new(vec3_new(-0.75f, -0.2f, 6.5f), 1.5f, red);
     spheres[1] = sphere_new(vec3_new(0, -1, 5), 1.0f, blue);
@@ -398,52 +388,59 @@ int main(void)
     spheres[3] = sphere_new(vec3_new(0, -4001, 0), 4000, ground);
 
     // Lights
-    #define NUM_LIGHTS 3
+#define NUM_LIGHTS 3
     Light lights[NUM_LIGHTS];
     lights[0] = light_ambient_new(0.2f);
     lights[1] = light_point_new(0.6f, vec3_new(-8, 1, 0));
     lights[2] = light_directional_new(0.2f, vec3_new(1, 4, -8));
 
     // For non-square images (future-proofing?)
-    float aspect_ratio = (float)WIDTH/(float)HEIGHT;
-    float screen_dim = tanf(FOV / (float)2);
+    float aspect_ratio = (float) WIDTH / (float) HEIGHT;
+    float screen_dim = tanf(FOV / (float) 2);
 
     Vec3 origin = Zero;
 
     // Render
-    for (int x = 0; x < WIDTH; x++) {
+    for (int x = 0; x < WIDTH; x++) {    // caching - ?
         for (int y = 0; y < HEIGHT; y++) {
 
-            // Background
-            data[(y*WIDTH + x) * 3 + 0] = (byte)(y/(float)WIDTH * 255);
-            data[(y*WIDTH + x) * 3 + 1] = (byte)(x/(float)HEIGHT * 255);
-            data[(y*WIDTH + x) * 3 + 2] = (byte)160;
-            
-            // Get Pixel in World Coords
-            float x_world_coord = (2*(x + 0.5f)/(float)HEIGHT - 1) * screen_dim * aspect_ratio;
-            float y_world_coord = -(2*(y + 0.5f)/(float)WIDTH - 1) * screen_dim;
-            Vec3 dir = vec3_normalise(vec3_new(x_world_coord, y_world_coord, 1));
+            double r = 0, g = 0, b = 0;
 
-            // Raytrace Pixel
-            RgbColour colour = raytrace(origin, dir, 1.0f, (float)MAX_DIST,
-                                        spheres, NUM_SPHERES,
-                                        lights, NUM_LIGHTS);
+            for (int s = 0; s < SAMPLES; s++) {
+                // Get Pixel in World Coords
+                float x_world_coord = (2 * (x + 0.5f) / (float) HEIGHT - 1) * screen_dim * aspect_ratio;
+                float y_world_coord = -(2 * (y + 0.5f) / (float) WIDTH - 1) * screen_dim;
+                Vec3 dir = vec3_normalise(vec3_new(x_world_coord, y_world_coord, 1));
 
-            // Draw Geometry
-            if (colour.x != -1) {
-                data[(y*WIDTH + x) * 3 + 0] = (byte)colour.x;
-                data[(y*WIDTH + x) * 3 + 1] = (byte)colour.y;
-                data[(y*WIDTH + x) * 3 + 2] = (byte)colour.z;
+                // Raytrace Pixel
+                RgbColour colour = raytrace(origin, dir, 1.0f, (float) MAX_DIST,
+                                            spheres, NUM_SPHERES,
+                                            lights, NUM_LIGHTS);
+
+                if (colour.x != -1) {
+                    r += (byte) colour.x ;
+                    g += (byte) colour.y ;
+                    b += (byte) colour.z ;
+                } else {
+                    r += (byte) (y / (float) WIDTH * 255 );
+                    g += (byte) (x / (float) HEIGHT * 255 );
+                    b += (byte) 160;
+                }
+
+                data[(y * WIDTH + x) * 3 + 0] = r  / SAMPLES;
+                data[(y * WIDTH + x) * 3 + 1] = g  / SAMPLES;
+                data[(y * WIDTH + x) * 3 + 2] = b  / SAMPLES;
             }
+
         }
     }
 
     // Output
-    DrawWatermark(data);
+//    DrawWatermark(data);
 
     // Write to file
     printf("tinyray: writing to file!");
-    if (!stbi_write_bmp("output.bmp", WIDTH, HEIGHT, 3, data))
+    if (!stbi_write_bmp("output-algo.bmp", WIDTH, HEIGHT, 3, data))
         printf("tinyray: failed to write image!");
 
     return 0;
